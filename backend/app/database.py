652eb29f3fa4cbe4e95c6b10fe50b13ac8db595b
OscarFdezS <oscar.fernandez.sebastian@alumnos.upm.es>
2025-11-15 19:36:33 +0100

    Adapt Backend to deploy (render.com)

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,5 @@
+"""
+# En Local:
 from sqlmodel import SQLModel, create_engine, Session
 import os
 
@@ -6,6 +8,33 @@ engine = create_engine(DATABASE_URL, echo=False)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+def get_session():
+    with Session(engine) as session:
+        yield session
+"""
+from sqlmodel import SQLModel, create_engine, Session
+import os
+
+# --- BASE PATH PERSISTENTE EN RENDER ---
+# Render ofrece /var/data como directorio persistente
+PERSISTENT_DIR = "/var/data"
+
+# Si existe /var/data (Render), guardamos la DB ahí
+if os.path.exists(PERSISTENT_DIR):
+    DATABASE_PATH = os.path.join(PERSISTENT_DIR, "db.sqlite")
+else:
+    # Local: guardamos en el directorio actual
+    DATABASE_PATH = "./db.sqlite"
+
+# Construimos la URL para SQLAlchemy
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 def get_session():
     with Session(engine) as session:
         yield session
