f405e7e9f47429c1ff75085271454806a23c359c
OscarFdezS <oscar.fernandez.sebastian@alumnos.upm.es>
2025-11-15 19:59:09 +0100

    Adapt Frontend to deploy (render.com)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API = axios.create({ baseURL: 'http://localhost:8000' })
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000' })
 export const fetchSessions = () => API.get('/sessions')
 export const createSession = (data) => API.post('/sessions', data)
 export const updateSession = (id, data) => API.put(`/sessions/${id}`, data)
